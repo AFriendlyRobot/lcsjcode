aa95880be0afd0616a4ad7c149cf6c6cf9c99ab8
Jake <jakemingolla@gmail.com>
2015-11-01 14:50:33 -0500

    fleshed out more of the body

--- routes/admin.js
+++ routes/admin.js
@@ -8,12 +8,20 @@ router.get('/', function(req, res, next) {
 
 /* POST add organization */
 router.post('/add/', function(req, res, next) {
-  res.send('Added organization');
+  if (req.body.name == null) {
+    res.send('Cannot add, missing name argument');
+  } else {
+    res.send('Added organization named "' + req.body.name + '"');
+  }
 });
 
 /* POST delete organization */
 router.post('/delete/', function(req, res, next) {
-  res.send('Deleted organization');
+  if (req.body.name == null) {
+    res.send('Cannot delete, missing name argument');
+  } else {
+    res.send('Deleted organization named "' + req.body.name + '"');
+  }
 });
 
 
