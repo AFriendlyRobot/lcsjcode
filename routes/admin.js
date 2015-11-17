f7d13b8bbf49609c07834c94afea786ecf86f1c9
Russell Parker <russell.parker7@gmail.com>
2015-11-16 21:35:48 -0500

    Logic to add organization to database

--- routes/admin.js
+++ routes/admin.js
@@ -10,9 +10,14 @@ router.get('/', function(req, res, next) {
 /* POST add organization */
 router.post('/add/', util.authenticateBody, function(req, res, next) {
   if (req.body.name == null) {
-    res.send('Cannot add, missing name argument');
+    return res.status(400).send('Cannot add, missing name argument');
   } else {
-    res.send('Added organization named "' + req.body.name + '"');
+    var organizations = req.db.get("organizations");
+    req.db.insert({name: util.sanitize(req.body.name)}, function(err, docs) {
+      if (err) {return res.status(500).send("Internal server error");}
+      return res.status(200).send('Added organization named "' + req.body.name + '"');
+    });
+    //res.send('Added organization named "' + req.body.name + '"');
   }
 });
 
@@ -21,10 +26,12 @@ router.post('/delete/', function(req, res, next) {
   if (req.body.name == null) {
     res.send('Cannot delete, missing name argument');
   } else {
+    
     res.send('Deleted organization named "' + req.body.name + '"');
   }
 });
 
+/* GET list of organizations */
 router.get('/list', function(req, res, next) {
   var organizations = req.db.get("organizations");
 
